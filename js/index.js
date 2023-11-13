2df2b41b61136ef19c8d392b964f2a1fa5d1847e
jacobtfn <jacob.tfn@gmail.com>
2023-11-13 23:16:55 +0000

    ts-first steps

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 "use strict";
 console.log("hello jcb, yuhu!");
+const hero = document.querySelector('.hero');
+const text = document.querySelector('h1');
+// const maxWalk = 200;
+const walk = 500; //px
+function shadow(e) {
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+    const { offsetTop, offsetLeft } = e.target;
+    // If mouse goes on a children element from the one listened it will restart coordinates inside that child element, it needs to be amended
+    if (this !== e.target) {
+        x = x + offsetLeft;
+        y = y + offsetTop;
+    }
+    ;
+    const xWalk = ((x / width * walk) - (walk / 2));
+    const yWalk = ((y / height * walk) - (walk / 2));
+    text.style.textShadow = `
+        ${xWalk}px ${yWalk}px 0 red,
+        ${xWalk * -0.5}px ${yWalk * -0.5}px 0 rgba(255, 0, 255, 0.7),
+        ${xWalk * -1}px ${yWalk}px 0 rgba(0, 255, 255, 0.7),
+        ${xWalk}px ${yWalk * -1}px 0 rgba(0, 255, 0, 0.7),
+        ${xWalk * -1}px ${yWalk * -1}px 0 rgba(0, 0, 255, 0.7)
+    `;
+}
+hero.addEventListener('mousemove', shadow);
 //# sourceMappingURL=index.js.map
